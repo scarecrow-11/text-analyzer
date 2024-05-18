c428f77ab4baa5616d4eba63fbdb49212ca6ddd1
scarecrow-11 <hafizurmilon.11@gmail.com>
2024-05-19 04:31:54 +0600

    [chore] add sentry error handler

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { NestFactory } from '@nestjs/core'
+import './instrument'
+import * as Sentry from '@sentry/node'
+import { NestFactory, HttpAdapterHost, BaseExceptionFilter } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 import { AppModule } from './app.module'
 
@@ -7,6 +9,11 @@ const PORT = process.env.PORT || 4000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
+
+  // Sentry error handling
+  const { httpAdapter } = app.get(HttpAdapterHost)
+  Sentry.setupNestErrorHandler(app, new BaseExceptionFilter(httpAdapter))
+
   await app.listen(PORT)
 }
 bootstrap()
